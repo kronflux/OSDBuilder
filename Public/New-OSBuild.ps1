c9cc68a7da8f10258741c698baab23f4fd7ce2fe
zottelbeyer <klaus.steinhauer@gmail.com>
2024-10-13 10:51:20 +0200

    New-OSBuild allow selection of content pack

--- Public/New-OSBuild.ps1
+++ Public/New-OSBuild.ps1
@@ -100,6 +100,9 @@ function New-OSBuild {
         [Parameter(ParameterSetName='Taskless')]
         [switch]$SelectContentPacks = $global:SetOSDBuilder.NewOSBuildSelectContentPacks,
 
+        #Preselect Content Pack
+        [String[]]$UseContentPacks,
+
         #Allows you to select Updates to apply in GridView
         #Useful for Testing
         [switch]$SelectUpdates = $global:SetOSDBuilder.NewOSBuildSelectUpdates,
@@ -436,6 +439,8 @@ function New-OSBuild {
                 if ($EnableNetFX.IsPresent) {$EnableNetFX3 = $true}
                 if ((Get-IsContentPacksEnabled) -and ($SelectContentPacks.IsPresent)) {
                     $ContentPacks = (Get-TaskContentPacks -Select).Name
+                } else {
+                    $ContentPacks = $UseContentPacks
                 }
                 Show-TaskInfo
             }
